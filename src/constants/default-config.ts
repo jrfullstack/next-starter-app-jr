ebb5fad3c5153038ec0a4f54f6116091d77b2139
Jimmy Reyes <jrfullstack@gmail.com>
2025-05-05 01:58:02 +0200

    feat: eliminar la propiedad contactEmail y agregar configuración SMTP en el archivo de configuración por defecto

--- src/constants/default-config.ts
+++ src/constants/default-config.ts
@@ -3,7 +3,6 @@ import type { ResolvedAppConfig } from "@/types";
 // Valores por defecto (debe satisfacer el tipo ResolvedAppConfig)
 export const defaultConfig: ResolvedAppConfig = {
   id: 1,
-  contactEmail: null,
   logoUrl: null,
   isMaintenanceMode: false,
   googleAnalyticsTrackingId: "",
@@ -22,6 +21,12 @@ export const defaultConfig: ResolvedAppConfig = {
   isSiteNoIndexEnabled: false,
   seoDefaultKeywords: [],
   faviconUrl: null,
+
+  // SMTP
+  emailHost: null,
+  emailPort: 587,
+  emailUser: "noreply@midominio.com",
+  isEmailConfigured: false,
 
   createdAt: new Date(),
   updatedAt: new Date(),
