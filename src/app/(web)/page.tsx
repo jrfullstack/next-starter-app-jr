5f4765e826f12d6b51986d24e5f51a12bd6039e6
Jimmy Reyes <jrfullstack@gmail.com>
2025-05-05 19:39:13 +0200

    feat: agregar navegación de usuario y mejorar la estructura de la página de inicio con nuevos enlaces y componentes

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 
 import { onSignOut } from "@/actions/auth/sign-out";
-import { Button, ModeToggle } from "@/components";
+import { Button, ModeToggle, UserNav } from "@/components";
 
 export default function Home() {
   const { data: session, update } = useSession();
@@ -23,15 +23,18 @@ export default function Home() {
     console.warn(session);
   };
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+    <div className="min-h-screen items-center justify-items-center gap-16 space-y-8 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <h1 className="text-3xl">Bienvenido a mi plantilla para proyectos con NextJS</h1>
-
-      <ModeToggle />
+      <div className="flex flex-row space-x-2">
+        <ModeToggle />
+        <UserNav />
+      </div>
       <div className="flex flex-col space-y-2">
         <Link href="/auth/signup">Regístrate</Link>
         <Link href="/auth/signin">Iniciar session</Link>
         <Link href="/auth/verify">Verificar el email</Link>
         <Link href="/auth/verify/resend">reenviar email de verificación</Link>
+        <Link href="/admin">Panel de Administración</Link>
         <Button onClick={onSignOut}>Salir</Button>
         <Button onClick={updateSession}>Update Session</Button>
         <Button onClick={logSession}>Log session</Button>
