f4a3a9862fa1c88d151ddc7ce15706876720af61
Jimmy Reyes <jrfullstack@gmail.com>
2025-05-12 11:59:52 +0200

    feat: actualizar el pie de página para reflejar el autor correcto y el enlace al repositorio del proyecto en español

--- src/components/admin/panel/footer.tsx
+++ src/components/admin/panel/footer.tsx
@@ -5,18 +5,18 @@ export function Footer() {
     <div className="bg-background/95 supports-[backdrop-filter]:bg-background/60 z-20 w-full shadow backdrop-blur">
       <div className="mx-4 flex h-14 items-center md:mx-8">
         <p className="text-muted-foreground text-left text-xs leading-loose md:text-sm">
-          Built on top of{" "}
+          Creado por{" "}
           <Link
-            href="https://ui.shadcn.com"
+            href="https://github.com/jrfullstack"
             target="_blank"
             rel="noopener noreferrer"
             className="font-medium underline underline-offset-4"
           >
-            shadcn/ui
+            JrFullStack
           </Link>
-          . The source code is available on{" "}
+          . El código fuente está disponible en{" "}
           <Link
-            href="https://github.com/salimi-my/shadcn-ui-sidebar"
+            href="https://github.com/jrfullstack/next-starter-app-jr"
             target="_blank"
             rel="noopener noreferrer"
             className="font-medium underline underline-offset-4"
