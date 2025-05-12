c245c6c8b4921ff338e7004a294c84ccf7308b0a
Jimmy Reyes <jrfullstack@gmail.com>
2025-05-12 11:59:42 +0200

    fix: corregir la visualización del logo en AuthLinkLogo para que solo se muestre si logoUrl está definido

--- src/components/auth/AuthLinkLogo.tsx
+++ src/components/auth/AuthLinkLogo.tsx
@@ -9,7 +9,7 @@ export const AuthLinkLogoAuth = ({ logoUrl, siteDisplayName }: Props) => {
   return (
     <Link href="/" className="flex items-center gap-2 self-center font-medium">
       <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
-        <Image src={logoUrl || ""} alt={siteDisplayName} width={20} height={20} />
+        {logoUrl && <Image src={logoUrl} alt={siteDisplayName} width={20} height={20} />}
       </div>
       {siteDisplayName}
     </Link>
